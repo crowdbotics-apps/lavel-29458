dfe8afaf8e5e20d583a51326c0eea6a1f503047c
Crowdbotics <team@crowdbotics.com>
2021-08-04 16:23:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lavel_29458Identifier",
+  url: "https://lavel_29458Identifier.botics.co",
+  api: "https://lavel_29458Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
